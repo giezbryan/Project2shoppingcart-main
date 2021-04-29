2dfdf5347bae6ba0b306d38995a6183e549b264c
Bryan Chua <bryanchua0724@gmail.com>
2021-04-29 19:58:41 +0800

    Init

---
+++ js/app.js
@@ -0,0 +1,42 @@
+
+const courses = document.querySelector('#courses-list');
+
+
+
+
+
+loadEventListeners();
+
+function loadEventListeners() {
+
+    courses.addEventListener('click, buyCourse');
+}
+
+
+
+
+
+
+
+
+
+function buyCourse(e) {
+    console.log('Course Added');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
